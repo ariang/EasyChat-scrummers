f714d5f314f9345db5c25b010bd465fb51a80680
Arian Gagica <gagicaarian@gmail.com>
2019-12-02 15:00:58 +0100

    try

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/easychat'));
+app.use(express.static(__dirname + '/dist/easychat'));
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, '/dist/easychat/index.html'));
+	res.sendFile(path.join(__dirname + '/dist/easychat/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
